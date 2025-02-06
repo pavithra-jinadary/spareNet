5e6366617e0a8c6fa1cc17e22e5c6b29e579f7bf
pavithra-jinadary <pavithrajinadary@gmail.com>
2025-02-06 10:27:01 +0530

    first commit

---
+++ src/main/java/Sparenetcom/Sparenet/SparenetApplication.java
@@ -0,0 +1,13 @@
+package Sparenetcom.Sparenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sparenetcom/Sparenet/SparenetApplicationTests.java
@@ -0,0 +1,13 @@
+package Sparenetcom.Sparenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
